ccb142c5d8bd75f9811bb8803502d605fe1ac826
rychagoff <coming.sites@gmail.com>
2024-03-20 02:16:56 +0400

    Прописывает логику для большой фотографии

--- js/modal-photo.js
+++ js/modal-photo.js
@@ -1,10 +1,15 @@
-// import { getPhotos } from './data.js';
 import { pictures } from './photo.js';
 
 const modalPicture = document.querySelector('.big-picture'); // Ищем модальное окно
-const modalPictureImg = modalPicture.querySelector('.big-picture__img').querySelector('img'); // Ищем изображение
-const modalPictureClose = modalPicture.querySelector('.big-picture__cancel'); // Ищем кнопку закрытия
-const miniaturePicture = document.querySelector('.picture');
+
+// Ищем его изображение и кнопку закрытия
+const modalPictureImg = modalPicture.querySelector('.big-picture__img').querySelector('img');
+const modalPictureClose = modalPicture.querySelector('.big-picture__cancel');
+
+const miniatures = document.querySelectorAll('.picture'); // Ищем миниатюры
+
+const arrayUrl = pictures.map((item) => item.url); // Получаем массив путей изображений
+// console.log(arrayUrl);
 
 const modalOpen = function() {
   modalPicture.classList.remove('hidden');
@@ -14,8 +19,21 @@ const modalClose = function() {
   modalPicture.classList.add('hidden');
 };
 
-miniaturePicture.addEventListener('click', () => {
-  modalOpen();
+// Пишем обработчик для каждой миниатюры
+// В параметрах миниатюра и элемент массива путей
+const miniatureClickHandler = function(miniature, arayUrlItem) {
+  miniature.addEventListener('click', () => {
+    modalOpen();
+    modalPictureImg.src = arayUrlItem; // Меняем путь большого изображения
+  });
+};
+
+// Перебор массива миниатюр
+// В параметрах миниатюра и её индекс
+miniatures.forEach((miniature, index) => {
+  // Индекс миниатюры передаем в параметр обработчику (как индекс пути),
+  // чтобы связать каждую миниатюру с каждым её путем
+  miniatureClickHandler(miniature, arrayUrl[index]);
 });
 
 modalPictureClose.addEventListener('click', () => {
